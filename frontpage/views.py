9e52b3cecfe46bf1a67a77e9140d6799ff585b47
Ash4059 <53855674+Ash4059@users.noreply.github.com>
2020-09-08 22:22:51 +0530

    Add files via upload

--- frontpage/views.py
+++ frontpage/views.py
@@ -3,26 +3,26 @@ from django.http import HttpResponse
 from . import feature_extraction
 import joblib
 import numpy as np
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'model')))
 # Create your views here.
 url=""
-model_reload=joblib.load('./model/Phishing_website_detection.pkl')
+model_reload=joblib.load('model.Phishing_website_detection.pkl')
 def index(request):
     return render(request,'index.html')
-def detect_website(request):
+def detectWebsite(request):
     if(request.method=="POST"):
         url=request.POST.get('url')
     X_new=feature_extraction.generate_data_set(url)
-    if(type(X_new)=='str'):
-        return render(request,'result.html',{'result':X_new})
-    else:
+    X_new = np.array(X_new).reshape(1,-1)
+    try:
+        prediction = model_reload.predict(X_new)
+        if prediction == -1:
+            result = "this is a Phishing Url"
+        else:
+            result = "this is Legitimate Url"
+    except:
+        result = "this is Phishing Url"
+    return render(request,'result.html',{'result':result})
             
-        X_new = np.array(X_new).reshape(1,-1)
-        try:
-            prediction = model_reload.predict(X_new)
-            if prediction == -1:
-                result = "this is a Phishing Url"
-            else:
-                result = "this is Legitimate Url"
-        except:
-            result = "this is Phishing Url"
-        return render(request,'result.html',{'result':result})
+        
