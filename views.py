cb00bd3f23219977083ae6291ccb52df7ce3ae3b
Ash4059 <53855674+Ash4059@users.noreply.github.com>
2020-09-08 22:35:54 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import feature_extraction
+import joblib
+import numpy as np
+# Create your views here.
+url=""
+model_reload=joblib.load('./model/Phishing_website_detection.pkl')
+def index(request):
+    return render(request,'index.html')
+def detectWebsite(request):
+    if(request.method=="POST"):
+        url=request.POST.get('url')
+    X_new=feature_extraction.generate_data_set(url)
+    X_new = np.array(X_new).reshape(1,-1)
+    try:
+        prediction = model_reload.predict(X_new)
+        if prediction == -1:
+            result = "this is a Phishing Url"
+        else:
+            result = "this is Legitimate Url"
+    except:
+        result = "this is Phishing Url"
+    return render(request,'result.html',{'result':result})
+            
+        
